168b1d2747bf7836b4a807bbe8267f6f68f36536
SeanKim <sean.kim84@gmail.com>
2018-08-21 16:53:55 +0700

    Configurate the File and App

---
+++ src/Components/App/AppPresenter.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+const AppPresenter = () => {
+    <Fragment>
+        
+    </Fragment>
+}
+
+export default AppPresenter;
---
+++ src/store.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Store = React.createContext(null);
+export default Store;
