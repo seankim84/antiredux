6e6588287731c2a2b832aab97b0bb3503db507ad
SeanKim <sean.kim84@gmail.com>
2018-08-21 17:47:43 +0700

    Create the Notification

--- src/Components/App/AppPresenter.js
+++ src/Components/App/AppPresenter.js
@@ -1,9 +1,14 @@
 import React, { Fragment } from 'react';
+import Flex from 'styled-flex-component';
 import Header from '../Header';
+import Notification from 'Components/Notification';
 
 const AppPresenter = () => (
     <Fragment>
         <Header />
+        <Flex alginCenter full column>
+            <Notification text={"hellow"} seen={false} />
+        </Flex>
     </Fragment>
 )
 
---
+++ src/Components/Notification/index.js
@@ -0,0 +1,2 @@
+import Notification from './NotificationContainer';
+export default Notification;
