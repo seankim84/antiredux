a51772c4a54faa422e1529d025f53318067caf0d
SeanKim <sean.kim84@gmail.com>
2018-08-22 09:07:20 +0700

    Consuming the Store

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,12 +3,22 @@ import AppPresenter from './AppPresenter';
 import Store from 'store';
 
 class AppContainer extends Component {
+    state = {
+         message: "Hellow"
+    }
+    componentDidMount = () => {
+        setTimeout(() => {
+            this.setState({
+                message: "bye"
+            })
+        },2000)
+    }
     render(){
-        return(
-            <Store.Provider>
-                <AppPresenter />
+        return( //Presenter안에 있는 것들은 store에 접근가능.
+            <Store.Provider value={this.state}>
+                <AppPresenter /> 
             </Store.Provider>
-        )
+        ) 
     }
 }
 
