ae8df132fdd4d1a6c860bf0806a24279cf52a680
SeanKim <sean.kim84@gmail.com>
2018-08-22 09:20:45 +0700

    Updating the Store

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,15 +3,23 @@ import AppPresenter from './AppPresenter';
 import Store from 'store';
 
 class AppContainer extends Component {
-    state = {
-         message: "Hellow"
-    }
-    componentDidMount = () => {
-        setTimeout(() => {
-            this.setState({
-                message: "bye"
-            })
-        },2000)
+    constructor(props){ // Provider에 포함시킬 함수는 반드시 constructor 안에 있어야한다.
+        super(props);
+        this._changeMessage = () => {
+            if(this.state.message === "Hellow"){
+                this.setState({
+                    message:"Bye Bye"
+                });
+            } else {
+                this.setState({
+                    message: "Hellow",
+                })
+            }
+        }
+        this.state = {
+            message: "Hellow",
+            changeMessage:this._changeMessage
+        };
     }
     render(){
         return( //Presenter안에 있는 것들은 store에 접근가능.
